ba092ae5485e59903beeea2be8ec8cf7318d2a65
Hannatty <hanatxy@gmail.com>
2022-03-24 11:35:48 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function App() {
+  
+  //States
+  const [real, setReal] = useState();
+  const handleRealChange = (e) => {
+    setReal(e.target.value);
+  }
+
+  const [tax, setTax] = useState();
+  const handleTaxChange = (e) => {
+    setTax(e.target.value);
+  }
+
+  const [dolar, setDolar] = useState();
+  
+  var today = new Date();
+  var formated_date = today.toLocaleDateString('pt-BR', {timeZone: 'UTC'});
+
+  //Functions
+  const handleCalculaClick = () => {
+    var conversion = (real/tax).toFixed(2)
+    setDolar(conversion)
+  }
+
+  const handleCleanClick =() =>{
+    setReal('')
+    setDolar('')
+    setTax('')
+  }
+
+  return(
+    <div className='container'>
+      <h1>Conversor de Câmbio (Real - Dólar)</h1>
+      <h3>Data da Cotação: {formated_date} </h3>
+      <>
+        <label for="realValue" >Valor em Real </label> 
+        <input id="realValue" className='inputStyle' value={real} type="number" onChange={handleRealChange}/> 
+        <label for="tax" >Câmbio </label> 
+        <input id = "tax" className='inputStyle' value={tax} type="number" onChange={handleTaxChange}/> <br/><br/>
+        <button className='buttonStyle' onClick={handleCalculaClick} >Calcular</button> <br/>
+        <button className='buttonStyle2' onClick={handleCleanClick} >Limpar</button><br/><br/>
+        <label for="dolarValue">Valor em Dólar </label> <br/>
+        <input id="dolarValue" className='inputStyle' readOnly value ={dolar} type="number" placeholder='U$:'/> <br/> <br/>
+      </>
     </div>
   );
 }
-
-export default App;
